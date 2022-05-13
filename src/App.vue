365f5bca722957432b5066d94e0e07d98f1c692a
muccidavide <davidemucci0804@gmail.com>
2022-05-13 11:03:18 +0200

    Initial Commit & add Bootstrap

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1 class="text-center">Start</h1>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+const bootstrap = require('bootstrap')
+Vue.use(bootstrap)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
